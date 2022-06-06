36b2028cb88f94da1057f6bbfdd2bcb57591a1d9
19102005 <19102005@ittelkom-pwt.ac.id>
2022-06-06 07:26:17 +0700

    module 6

---
+++ Module6 - Copy/app/src/main/java/com/example/module6/DataClassWisata.kt
@@ -0,0 +1,10 @@
+package com.example.module6
+
+
+data class DataClassWisata(
+    var namaWisata: String ="",
+    var deskripsi: String ="",
+    var sampleImage: Int = 0,
+    var lat: Double = 0.0,
+    var long: Double = 0.0
+)
