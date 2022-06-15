e2d8023232c538d253d967a73c5395becc7eb42e
19102005 <19102005@ittelkom-pwt.ac.id>
2022-06-15 22:20:19 +0700

    update api

---
+++ rv_recyclerview/app/src/main/java/com/example/rv_recyclerview/dataclass/RestAPIDataClass.kt
@@ -0,0 +1,7 @@
+package com.example.rv_recyclerview.dataclass
+
+data class RestAPIDataClass(
+    var id: Int = 0,
+    var nama: String ="",
+    var umur: Int = 0
+)
---
+++ rv_recyclerview/app/src/main/java/com/example/rv_recyclerview/network/Repository.kt
@@ -0,0 +1,31 @@
+package com.example.rv_recyclerview.network
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+const val BASEURL ="http://10.212.9.208/REST_API_CI/index.php/ApiKu"
+class Repository {
+    companion object{
+        private var retrofit: Retrofit? =null
+        fun getDataAPI(): Retrofit{
+            val gson = GsonBuilder()
+                .setLenient()
+                .create()
+            val okHttpClient = OkHttpClient.Builder()
+                .readTimeout(100, TimeUnit.SECONDS)
+                .connectTimeout(100, TimeUnit.SECONDS)
+                .build()
+            if (retrofit==null){
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASEURL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build()
+            }
+            return retrofit!!
+        }
+    }
+}
